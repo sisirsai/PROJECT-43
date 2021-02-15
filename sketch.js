eaf27f41dba34757349a7d68091aa821a36d23f4
SISIR SAI <73339853+sisirsai@users.noreply.github.com>
2021-02-15 14:01:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ angleMode(DEGREES);
 
 secAngle = map(sec,0,60,0,360);
 minAngle = map(min,0,60,0,360);
-hrAngle = map(hr%12,0,12,-0,360);
+hrAngle = map(hr%12,0,12,-0,360);1
 
 }
 
@@ -57,4 +57,21 @@ stroke(0);
   line(0,0,80,0);
   pop();
 
+  textSize(35);
+  text("TICK TOK",500,500);
+   textSize(30);
+  text("TICK TOK",500,450);
+   textSize(25);
+  text("TICK TOK",500,400);
+   textSize(20);
+  text("TICK TOK",500,350);
+   textSize(15);
+  text("TICK TOK",500,300);
+   textSize(10);
+  text("TICK TOK",500,250);
+   textSize(5);
+  text("TICK TOK",500,200);
+   textSize(0);
+  text("TICK TOK",500,150);
+  
 }
